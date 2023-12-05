15bd35ecebccca29b90a9288ed572906b3e5d944
JeremyVanGrack <Jeremyv1@umbc.edu>
2023-12-04 19:25:19 -0500

    working pt 2

--- Day4_Scratchcards.py
+++ Day4_Scratchcards.py
@@ -2,21 +2,27 @@ INPUT_FILE = "Inputs/Day4_Input.txt"
 
 def cards(input):
     for line in input:
+        id = (line.split(": ")[0]).split()[1]
+        # print(id)
         M = [x.split() for x in ( (line.split(": ")[1]).split(" | "))]
         try:
             arr.append(M)
         except:
             arr = [M]
     
-    count = 0
+    sum = 0
     for i in arr:
+        count = 0
         for k in i[1]:
             if k in i[0]:
                 if count == 0:
                     count += 1
                 else:
                     count *= 2
-    print(count)
+        # print(count)
+        sum += count
+        
+    print(sum)
 
 def main():
     f = open(INPUT_FILE, "r")
