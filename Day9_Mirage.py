c8566e5b3051172daea74f9e6c506b59d23d0f0b
JeremyVanGrack <Jeremyv1@umbc.edu>
2023-12-10 15:54:51 -0500

    same answer :(

--- Day9_Mirage.py
+++ Day9_Mirage.py
@@ -2,35 +2,32 @@ import numpy as np
 #2075724809 too high, 989384392 too low
 INPUT_FILE = "Inputs/Day9_Input.txt"
 
+def getDiff(arr):
+    return [(int(j)-int(i)) for i, j in zip(arr[:-1], arr[1:])]
+
 def readSequences(lines):
     ans = 0
     for line in lines:
         line = line.split()
         memo = {}
-        arr = [(int(j)-int(i)) for i, j in zip(line[:-1], line[1:])]
-        memo[0] = arr[len(arr)-1]
-        i = 1
-        print(arr)
+        k = 0
+        diff = getDiff(line)
         while True:
-
-            arr = [(int(j)-int(i)) for i, j in zip(arr[:-1], arr[1:])]
-            memo[i] = arr[len(arr)-1]
-            i += 1
-            print(arr)
-            if sum(arr) == 0:
+            if (sum(diff) == 0):
                 break
+            else:
+                memo[k] = diff
+                diff = getDiff(memo[k])
 
-        # print(arr, memo)
-        memo.pop(len(memo) - 1)
-        arr = [0]
-        for key in range(len(memo)):
-            arr.append(memo[len(memo) - key - 1] + arr[len(arr) - 1])
-        
-        # print(arr)
-        print(int(line[len(line) - 1]), arr[len(arr) - 1])
-        ans += (int(line[len(line) - 1]) + arr[len(arr) - 1])
-    print(ans) 
-                
+            k += 1
+            
+        for key in range(len((memo.keys())) - 1, 0, -1):
+            memo[key - 1].append(memo[key][len(memo[key]) - 1] + memo[ key - 1 ][ len( memo[key - 1] ) - 1 ])
+            
+        line.append(memo[0][len(memo[0]) - 1] + int(line[len(line)-1]))
+        ans += line[len(line) - 1]
+    print(ans)
+
 
 def main():
     f = open(INPUT_FILE, "r")
